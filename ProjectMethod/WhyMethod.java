a6b78b5f455c6d7e957782eea839332d72f33595
Hyeonseong Lee <hslee7231@gmail.com>
2022-08-03 21:00:41 +0900

    메소드의 입력

--- ProjectMethod/WhyMethod.java
+++ ProjectMethod/WhyMethod.java
@@ -1,37 +1,21 @@
 
 public class WhyMethod {
-	public static void printTwoTimesA() {	// 메소드 생성
-		// 1억줄의 코드
-		System.out.println("-");	//
-		System.out.println("a");	// 동일한 코드 그룹핑
-		System.out.println("a");	//
-	}
 
 	public static void main(String[] args) {
-		// 메소드의 기본 형식
-		// 똑같은 코드를 여러 번 사용을 할 때, 중복이 일어나게 되고, 그 코드를 수정할 시에는 모든 중복 코드를 일일이 수정을 해야한다.
-		// 이러한 경우 서로 연관된 코드들을 그룹핑하여 메소드를 만들어준다.
 		
+					// 인자, argument
+		printTwoTimes("a", "-");	// 메소드를 호출할 때 메소드 옆 괄호 안에 인자를 넣어준다.
+		printTwoTimes("a", "*");	// 괄호 안에 , 찍으면 여러 개의 인자를 전달할 수 있다.
+		printTwoTimes("a", "&");
+		printTwoTimes("b", "!");
+		
+	}
+									// 매개변수, parameter
+	public static void printTwoTimes(String text, String delimiter) {	// 메소드 옆 괄호 안에 매개변수를 넣어준다.
 		// 1억줄의 코드
-		System.out.println("-");	//
-		System.out.println("A");	// 동일한 코드
-		System.out.println("A");	//
-		// 1억줄의 코드
-		System.out.println("-");	//
-		System.out.println("A");	// 동일한 코드
-		System.out.println("A");	//
-		// 1억줄의 코드				
-		System.out.println("-");	//
-		System.out.println("A");	// 동일한 코드
-		System.out.println("A");	//
-		
-		
-		printTwoTimesA();	// 동일한 코드를 그룹핑한 그룹 호출
-		printTwoTimesA();	// 동일한 코드를 그룹핑한 그룹 호출
-		printTwoTimesA();	// 동일한 코드를 그룹핑한 그룹 호출
-		
-		// 위의 코드들과 아래의 코드들은 동일한 동작을 한다.
-		
+		System.out.println(delimiter);	// 두 번째로 전달된 인자를 출력한다.
+		System.out.println(text);	// 매개 변수로 입력된 인자를 출력한다.
+		System.out.println(text);	
 	}
 
 }
