35e0b35eaabc453fc52ecb95a0bec5c244cd41d3
Hyeonseong Lee <hslee7231@gmail.com>
2022-08-03 19:43:14 +0900

    메소드란

---
+++ ProjectMethod/FirstMethod.java
@@ -0,0 +1,10 @@
+
+public class FirstMethod {
+	public static void main(String[] args) {
+		
+		// 메소드란 하나의 기능을 위한 코드를 그룹핑한 것, 메소드를 만들어서 그 기능이 필요할 때마다 해당 메소드만을 호출하면 됨 (중복의 제거)
+		System.out.println("Hello Method");	// Hello Method 문자열 출력
+		System.out.println(Math.floor(1.1));	// Java API중 수학과 관련된 클래스인 Math의 반내림을 해주는 메소드인 floor() 메소드 호출
+		
+	}
+}
